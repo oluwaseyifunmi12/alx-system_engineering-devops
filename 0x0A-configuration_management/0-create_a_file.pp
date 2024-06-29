ab48dacd01100e956d607c9e7e8c989c1dc600ae
Daramola Victoria <victoria.mnpadm@gmail.com>
2024-06-28 21:29:58 -0700

    puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # This is a puppet manifeat that creates a file in a /tmp folder
 
 file { '/tmp/school':
-
+  ensure  => present,
   owner   => 'www.data',
   group   => 'www.data',
   mode    => '0744',
